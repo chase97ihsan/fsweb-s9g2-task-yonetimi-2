446f44765d614dbb9068320ed5c96d56c05a2118
İhsan Doğan <ihsannote3@gmail.com>
2023-06-21 00:51:55 +0300

    yeni1

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,35 @@
+import { differenceInDays, formatDistanceToNow } from "date-fns";
+import { bg, tr } from "date-fns/locale";
 import React from "react";
 
 const Task = ({ taskObj, onComplete }) => {
+  const distance = formatDistanceToNow(new Date(taskObj.deadline), {
+    addSuffix: true,
+    locale: tr,
+  });
+  const differencee =
+    differenceInDays(new Date(taskObj.deadline), new Date()) <= 3;
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">
+        son teslim:{" "}
+        <span className={differencee ? "bg-ihsan" : "bg-doğan"}>
+          {distance}
+        </span>
+      </div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
-          <span className="pill" key={p}>{p}</span>
+          <span className="pill" key={p}>
+            {p}
+          </span>
         ))}
       </div>
-      {onComplete && <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>}
+      {onComplete && (
+        <button onClick={() => onComplete(taskObj.id)}>Tamamlandı</button>
+      )}
     </div>
   );
 };
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        ihsan: "#d4d7ff",
+        doğan: "#d4d7ff",
+      },
+    },
+  },
+  plugins: [],
+};
